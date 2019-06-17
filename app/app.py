aefab8542a114783abf8bef69846eaf3d71adb6b
Gustavo T Rudiger <gustavo.rudiger@ufpr.br>
2019-06-16 23:51:38 -0300

    Etapa 1, item 1 90%

--- app/app.py
+++ app/app.py
@@ -8,17 +8,41 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri =  "mongodb://host.docker.internal:27017/gustavorudiger")
 #mongo = PyMongo(app, uri =  "mongodb://127.0.0.1:27017/gustavorudiger")
 
+def get_item(obj):
+    return {
+        'nome' : obj['nome'], 
+        'idade_ate_31_12_2016' : obj['idade_ate_31_12_2016'], 
+        'ra' : obj['ra'], 
+        'campus' : obj['campus'], 
+        'municipio' : obj['municipio'], 
+        'curso' : obj['curso'],
+        'modalidade' : obj['modalidade'],
+        'nivel_do_curso' : obj['nivel_do_curso'],
+        'data_inicio' : obj['data_inicio']
+        }
+
 @app.route('/')
 def hello():
     return render_template('app.html')
 
 @app.route('/api/v1.0/estudantes', methods=['GET'])
 def get_estudantes():
-    estudantes = mongo.db.estudantes
+    modalidade = request.args.get('modalidade', default = None)
+    inicio = request.args.get('inicio', default = None)
+    params = {}
+    
+    if(modalidade is not None):
+        params['modalidade'] = modalidade
+    
+    if(inicio is not None):
+        params['data_inicio'] = { '$gt': inicio }
+
+    estudantes = mongo.db.estudantes.find(params).sort("_id", -1)
     output = []
-    for e in estudantes.find():
-        output.append({'nome' : e['nome'], 'ra' : e['ra']})
-    return jsonify({'result' : output})
+    for e in estudantes:
+        output.append(get_item(e))
+    return jsonify({'result': output})
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
